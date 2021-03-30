46f26f606155ae7260a94ad12471bf90b9376a77
bhoomi <chavda.bhoomi@gmail.com>
2021-03-30 17:37:56 +0530

    Add timing and pricing to get services api.

--- app/Libraries/serviceHelper.php
+++ app/Libraries/serviceHelper.php
@@ -8,8 +8,8 @@ class serviceHelper
 {
     public static function getAllService($request)
     {
-        $therapies = Therapy::where('shop_id', $request->get('shop_id'))->get();
-        $massages  = Massage::where('shop_id', $request->get('shop_id'))->get();
+        $therapies = Therapy::with('timing','pricing')->where('shop_id', $request->get('shop_id'))->get();
+        $massages  = Massage::with('timing','pricing')->where('shop_id', $request->get('shop_id'))->get();
         $services  = collect();
 
         foreach ($therapies as $therapy) {
