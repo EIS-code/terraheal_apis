7bd3d8d0324e8dd8c3186f2846b97a52af694c22
JaydeepMor <it.jaydeep.mor@gmail.com>
2021-05-04 10:55:05 +0530

    Debug

--- app/Http/Controllers/Massage/MassageController.php
+++ app/Http/Controllers/Massage/MassageController.php
@@ -87,7 +87,12 @@ class MassageController extends BaseController
     public function getMassageCenters(array $data = [], $isApi = true, int $limit = 10)
     {
         $request   = request();
+        echo "<pre>";
+        print_r($data);
         $data      = $request->all();
+        echo "<pre>";
+        print_r($data);
+        exit;
         $latitude  = (!empty($data['latitude'])) ? $data['latitude'] : $request->get('latitude', NULL);
         $longitude = (!empty($data['longitude'])) ? $data['longitude'] : $request->get('longitude', NULL);
         $limit     = (!is_numeric($limit)) ? 10 : $limit;
