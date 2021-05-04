84fffbd0a3975a7e252e084550448d06b475beb8
bhoomi <chavda.bhoomi@gmail.com>
2021-05-04 13:33:20 +0530

    change the migration.

--- database/migrations/2021_05_03_070442_add_image_column_to_shops_table.php
+++ database/migrations/2021_05_03_070442_add_image_column_to_shops_table.php
@@ -18,7 +18,6 @@ class AddImageColumnToShopsTable extends Migration
             $table->dropColumn('close_time');
             $table->dropColumn('open_day_from');
             $table->dropColumn('open_day_to');
-            $table->string('featured_image')->nullable()->after('name');
             $table->string('owner_mobile_number_alternative', 50)->nullable()->after('owner_mobile_number');
             $table->string('owner_surname')->nullable()->after('owner_name');
             $table->string('finacial_situation')->nullable()->after('owner_email');
@@ -36,7 +35,6 @@ class AddImageColumnToShopsTable extends Migration
             $table->dropColumn('owner_mobile_number_alternative');
             $table->dropColumn('owner_surname');
             $table->dropColumn('finacial_situation');
-            $table->dropColumn('featured_image');
             $table->time('open_time')->nullable();
             $table->time('close_time')->nullable();
             $table->enum('open_day_from', [0, 1, 2, 3, 4, 5, 6])->comment('0: Monday, 1: Tuesday, 2: Wednesday, 3: Thursday, 4: Friday, 5: Saturday, 6: Sunday')->nullable();
