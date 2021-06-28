1beeacf8a75d3e8e4c7c9fb796ae70f9090b1667
bhoomi <chavda.bhoomi@gmail.com>
2021-06-28 10:34:16 +0530

    debug.

--- app/Http/Controllers/Therapist/TherapistController.php
+++ app/Http/Controllers/Therapist/TherapistController.php
@@ -137,7 +137,7 @@ class TherapistController extends BaseController
 
         if (!empty($email) && !empty($password)) {
             $getTherapist = $model->where(['email' => $email, 'is_freelancer' => (string)$isFreelancer, 'active_app' => Therapist::IS_ACTIVE])->first();
-
+            dd($getTherapist);
             if (!empty($getTherapist) && Hash::check($password, $getTherapist->password)) {
                 $getTherapist = $getTherapist->first();
 
