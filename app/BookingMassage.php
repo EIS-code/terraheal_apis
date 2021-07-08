87a0b205149ffec0dde366d3702df6f8943190fc
bhoomi <chavda.bhoomi@gmail.com>
2021-07-08 15:29:08 +0530

    add massage date time

--- app/BookingMassage.php
+++ app/BookingMassage.php
@@ -14,6 +14,7 @@ class BookingMassage extends BaseModel
     use SoftDeletes;
     
     protected $fillable = [
+        'massage_date_time',
         'price',
         'cost',
         'origional_price',
