bdfbaeb55a018269ab08a2d28d8b14af278f40cf
bhoomi <chavda.bhoomi5@gmail.com>
2021-09-29 13:07:21 +0530

    debug revert

--- app/Therapist.php
+++ app/Therapist.php
@@ -248,8 +248,7 @@ class Therapist extends BaseModel implements CanResetPasswordContract
                       ->leftJoin($modelCity::getTableName(), $model::getTableName() . '.city_id', '=', $modelCity::getTableName() . '.id')
                       ->where($model::getTableName() . '.id', $id)
                       ->groupBy($model::getTableName() . '.id')
-                      ->toSql();
-        dd($data);
+                      ->get();
 
         if (!empty($data) && !$data->isEmpty($data)) {
             $bookingInfo = new BookingInfo();
