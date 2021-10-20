ce5417f0fb50b990ef6cc7430102ef1757de3a7e
bhoomi <chavda.bhoomi5@gmail.com>
2021-10-20 14:44:04 +0530

    Remove stripe routes

--- routes/web.php
+++ routes/web.php
@@ -73,5 +73,4 @@ $router->group(['middleware' => ['auth']], function () use($router) {
     include("shops.php");
     include("superAdmin.php");
     include("notification.php");
-    include("stripe.php");
 });
