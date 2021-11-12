804dfb41ac970b967a7788c48a9331a2639771e4
bhoomi <chavda.bhoomi5@gmail.com>
2021-11-12 12:21:04 +0530

    Add filter to get packs API

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -1316,68 +1316,40 @@ class UserController extends BaseController
 
         return $this->returns('success.user.faq.not.found', collect([]));
     }
-
-    public function myPacks(Request $request) {
-        $packs = UserPack::with('pack')->where(['user_id' => $request->user_id])->get();
-        $packData = [];
-        if (!empty($packs)) {
-            
-            foreach ($packs as $key => $value) {
-             
-                $packData[] = [
-                    "id" => $value->id,
-                    "pack_id" => $value->pack_id,
-                    "name" => $value->pack->name,
-                    "sub_title" => $value->pack->sub_title,
-                    "number" => $value->pack->number,
-                    "image" => $value->pack->image,
-                    "total_price" => $value->pack->total_price,
-                    "pack_price" => $value->pack->pack_price,
-                    "expired_date" => $value->pack->expired_date,
-                    "receptionist_id" => $value->pack->receptionist_id,
-                    "is_personalized" => $value->pack->is_personalized,
-                ];
-            }
-        }
-        return $packData;
-    }
-    
-    public function myGiftPacks(Request $request) {
-        $packs = UserPackGift::with('pack')->where('user_id', $request->user_id)->get();
-        $packData = [];
-        if (!empty($packs)) {
-            
-            foreach ($packs as $key => $value) {
-             
-                $packData[] = [
-                    "id" => $value->id,
-                    "pack_id" => $value->pack_id,
-                    "name" => $value->pack->name,
-                    "sub_title" => $value->pack->sub_title,
-                    "number" => $value->pack->number,
-                    "image" => $value->pack->image,
-                    "total_price" => $value->pack->total_price,
-                    "pack_price" => $value->pack->pack_price,
-                    "expired_date" => $value->pack->expired_date,
-                    "receptionist_id" => $value->pack->receptionist_id,
-                    "is_personalized" => $value->pack->is_personalized,
-                ];
-            }
-        }
-        return $packData;
-    }
     
     public function getPacks(Request $request)
     {
         $filter = !empty($request->filter) ? $request->filter : Pack::MY_PACKS;
         
         if($filter == Pack::MY_PACKS) {
-            $packs = $this->myPacks($request);
+            $packs = UserPack::with('pack')->where(['user_id' => $request->user_id])->get();
         } else {
-            $packs = $this->myGiftPacks($request);
+            $packs = UserPackGift::with('pack')->where('user_id', $request->user_id)->get();
         }
-        
-        return $this->returns('success.user.packs.found', collect($packs));
+                
+        if (!empty($packs)) {
+            
+            $packData = [];
+            foreach ($packs as $key => $value) {
+             
+                $packData[] = [
+                    "id" => $value->id,
+                    "pack_id" => $value->pack_id,
+                    "name" => $value->pack->name,
+                    "sub_title" => $value->pack->sub_title,
+                    "number" => $value->pack->number,
+                    "image" => $value->pack->image,
+                    "total_price" => $value->pack->total_price,
+                    "pack_price" => $value->pack->pack_price,
+                    "expired_date" => $value->pack->expired_date,
+                    "receptionist_id" => $value->pack->receptionist_id,
+                    "is_personalized" => $value->pack->is_personalized,
+                ];
+            }
+            return $this->returns('success.user.packs.found', collect($packData));
+        }
+
+        return $this->returns('success.user.packs.not.found', collect([]));
     }
        
     public function getPackServices(Request $request)
